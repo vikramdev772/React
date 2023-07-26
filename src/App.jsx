f7b361e2a2333b7a6a79586998a1bb8db65c9211
vikram <vikramdev772@gmail.com>
2023-07-26 23:30:56 +0530

    feat:  mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {Button,Card,Typography} from '@mui/material'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div style={{display:"flex",justifyContent:"center"}}>
-      <Card style={{padding: 20 , width: 500}}>
-        <Typography variant="h5">
-          Welcome to counter game
-          </Typography>
-          <br/>
-          <Button count={count} setCount={setCount}/> 
-        </Card> 
-
-
-    </div>
-      
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Card style={{ padding: 20, width: 500 }}>
+          <Typography variant="h5">Welcome to counter game</Typography>
+          <br />
+          <Buttons count={count} setCount={setCount} />
+        </Card>
+      </div>
     </>
-  )
+  );
 }
 
-function Button({count,setCount}){
-
-  return <>
-    <div style={{display:"flex",justifyContent:"space-between"}}>
-      <Increase count = {count} setCount ={setCount}/>
-      <Decrease count = {count} setCount ={setCount}/>
-
-    </div>
-  
-  </>
+function Buttons({ count, setCount }) {
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Increase count={count} setCount={setCount} />
+        <Decrease count={count} setCount={setCount} />
+      </div>
+    </>
+  );
 }
 
-function Increase({count,setCount}){
-  return <>
-  <div>
-  <Button variant={"contained"} onClick={()=>{
-    setCount(count+1) 
-  }}>
-    Increase counter
-  </Button>
-    
-  </div>
-  
-  </>
+function Increase({ count, setCount }) {
+  return (
+    <>
+      <div>
+        <Button
+          variant={"contained"}
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Increase counter
+        </Button>
+      </div>
+    </>
+  );
 }
 
-function Decrease({count,setCount}){
-  return <>
-  <div>
-  <Button variant={"contained"} onClick={()=>{
-    setCount(count-1) 
-  }}>
-    Decrease counter
-  </Button>
-    
-  </div>
-  
-  </>
+function Decrease({ count, setCount }) {
+  return (
+    <>
+      <div>
+        <Button
+          variant={"contained"}
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          Decrease counter
+        </Button>
+      </div>
+    </>
+  );
 }
 
-
-export default App
-
-
+export default App;
