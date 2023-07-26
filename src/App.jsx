136bc5ed90c066f56c5c3970c6abf18005cfa414
vikram <vikramdev772@gmail.com>
2023-07-26 23:18:57 +0530

    Counter Button

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,38 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
 
+function Button({count,setCount}){
+
+  return <>
+    <div style={{display:"flex",justifyContent:"space-between"}}>
+      <Increase count = {count} setCount ={setCount}/>
+      <Dcrease count = {count} setCount ={setCount}/>
+
+    </div>
+  
+  </>
+}
+
+function Increase({count,setCount}){
+  return <>
+  <div>
+  <Button variant={"contained"} onClick={()=>{
+    setCount(count+1) 
+  }}>
+    Increase counter
+  </Button>
+    
+  </div>
+  
+  </>
+}
+
+
 export default App
+
+
