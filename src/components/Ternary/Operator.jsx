b372bec87f3ab9ba26b89d58278aee99ddf2b766
vikramdev772 <vikramdev772@gmail.com>
2024-03-26 15:18:09 +0530

    Ternary Operator

--- src/components/Ternary/Operator.jsx
+++ src/components/Ternary/Operator.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { useState } from 'react';
 
 const Operator = () => {
-    const [fruit ,setFruit] = useState(true);
+    const [fruit ,setFruit] = useState(false);
     console.log(fruit)
 
   return (
     <div>
         <center>
-            <h1> {fruit?`apple`:`mango `}</h1>
-            <h1>i eat {fruit}</h1>
+            <h1> i eat {fruit?`apple`:`mango `}</h1>
+            
         </center>
     </div>
   )
